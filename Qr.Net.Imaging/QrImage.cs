7dcb3e9bbb4f8a1560861a0b6e0362f3b31c268f
Aaron Ogan <aaron.ogan@gmail.com>
2011-07-14 20:54:41 -0500

    Removing old sample projects, adding test project, and first go at separating encoding and imaging logic.

---
+++ Qr.Net.Imaging/QrImage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Qr.Net.Imaging
+{
+    public class QrImage
+    {
+        protected int qrCodeScale = 4;
+        protected Color qrCodeBackgroundColor = Color.White;
+        protected Color qrCodeForegroundColor = Color.Black;
+
+        public Bitmap CreateImage(bool[][] matrix)
+        {
+            SolidBrush brush = new SolidBrush(qrCodeBackgroundColor);
+            Bitmap image = new Bitmap((matrix.Length * qrCodeScale) + 1, (matrix.Length * qrCodeScale) + 1);
+            Graphics g = Graphics.FromImage(image);
+            g.FillRectangle(brush, new Rectangle(0, 0, image.Width, image.Height));
+            brush.Color = qrCodeForegroundColor;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    if (matrix[j][i])
+                    {
+                        g.FillRectangle(brush, j * qrCodeScale, i * qrCodeScale, qrCodeScale, qrCodeScale);
+                    }
+                }
+            }
+
+            return image;
+        }
+    }
+}
---
+++ Qr.Net.Tests/QrCodeEncoderTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThoughtWorks.QRCode.Codec;
+
+namespace Qr.Net.Tests
+{
+    [TestClass]
+    public class QrCodeEncoderTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var encoder = new QRCodeEncoder();
+            var content = "QR.NET https://github.com/aaronogan/QR.NET";
+
+            var result = encoder.Encode(content);
+
+            AssertMatrix(result);
+        }
+
+        private void AssertMatrix(bool[][] matrix)
+        {
+            for (int x = 0; x < expected.Length; x++)
+            {
+                for (int y = 0; y < expected[0].Length; y++)
+                {
+                    Assert.AreEqual<bool>(expected[x][y], matrix[x][y]);
+                }
+            }
+        }
+
+        private bool[][] expected = new bool[][]
+        {
+            new bool[] { true, true, true, true, true, true, true, false, true, true, true, false, true, false, true, true, true, false, true, false, false, true, true, false, true, false, false, true, true, false, false, true, false, false, false, false, true, false, true, true, true, true, true, true, true },
+            new bool[] { true, false, false, false, false, false, true, false, false, false, true, false, false, true, false, true, true, false, true, true, true, true, false, true, false, true, false, true, false, true, false, true, false, false, false, true, false, false, true, false, false, false, false, false, true },
+            new bool[] { true, false, true, true, true, false, true, false, false, true, false, true, true, false, true, false, false, false, true, true, true, true, true, false, false, false, false, true, true, true, false, true, true, true, false, true, false, false, true, false, true, true, true, false, true },
+            new bool[] { true, false, true, true, true, false, true, false, false, true, true, true, true, false, false, true, false, false, false, true, false, true, true, false, true, true, false, true, true, false, true, true, true, false, false, true, true, false, true, false, true, true, true, false, true },
+            new bool[] { true, false, true, true, true, false, true, false, true, false, true, false, true, false, true, false, false, false, false, false, true, true, true, true, true, false, true, true, true, true, false, true, false, true, true, true, true, false, true, false, true, true, true, false, true },
+            new bool[] { true, false, false, false, false, false, true, false, false, true, true, false, false, false, false, true, true, true, false, false, true, false, false, false, true, false, true, true, true, false, true, false, true, true, false, false, false, false, true, false, false, false, false, false, true },
+            new bool[] { true, true, true, true, true, true, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, true, true, true, true, true, true },
+            new bool[] { false, false, false, false, false, false, false, false, true, false, false, false, false, true, true, false, false, true, true, true, true, false, false, false, true, false, false, true, false, false, true, false, true, true, true, true, false, false, false, false, false, false, false, false, false },
+            new bool[] { true, false, false, true, true, true, true, true, true, true, false, true, false, false, false, false, true, true, false, true, true, true, true, true, true, false, true, false, true, false, false, true, true, false, true, true, false, true, true, false, true, false, false, false, true },
+            new bool[] { true, true, true, false, true, true, false, true, false, false, false, true, false, true, true, true, true, false, false, true, false, false, false, true, false, false, true, true, false, false, false, false, true, true, true, true, false, false, true, true, true, false, true, true, true },
+            new bool[] { true, true, true, true, false, false, true, false, false, true, true, true, false, true, true, true, true, false, true, true, false, true, false, true, false, false, false, true, false, true, true, true, false, true, true, true, true, true, false, true, true, true, true, false, true },
+            new bool[] { true, false, true, true, true, false, false, true, true, true, false, false, true, true, true, true, true, false, false, false, true, true, true, false, false, true, false, false, true, true, true, true, false, false, true, false, true, true, true, true, false, false, false, true, true },
+            new bool[] { false, true, true, false, true, false, true, false, true, false, false, false, true, false, true, true, true, true, false, false, false, true, false, true, false, false, false, false, true, false, false, true, true, false, false, true, false, true, false, true, true, false, false, false, false },
+            new bool[] { true, false, false, true, false, true, false, true, true, false, false, false, false, true, true, true, true, true, true, true, true, false, true, true, true, false, false, false, false, false, true, true, false, true, false, false, true, false, false, true, true, true, true, false, false },
+            new bool[] { false, false, false, false, true, true, true, true, false, false, true, true, true, false, true, false, true, false, true, true, true, true, false, true, true, false, false, true, false, true, true, true, false, true, false, true, false, true, true, false, true, true, true, false, false },
+            new bool[] { false, false, true, true, false, false, false, true, false, true, false, true, false, true, true, true, false, true, true, false, false, true, false, false, true, false, false, true, false, false, false, true, true, true, false, true, false, true, false, true, false, true, true, false, false },
+            new bool[] { false, false, true, false, true, true, true, true, false, false, false, true, false, false, false, false, false, true, true, false, false, false, false, false, false, false, true, false, false, true, true, true, true, false, true, false, true, false, true, true, false, false, true, true, false },
+            new bool[] { false, false, false, true, true, true, false, true, true, false, true, true, true, false, true, false, false, true, false, true, true, false, true, false, false, true, false, true, true, true, true, true, false, false, true, true, false, true, true, false, false, false, false, false, true },
+            new bool[] { true, true, false, false, true, false, true, true, false, true, false, true, true, false, false, false, true, true, true, true, false, true, false, false, true, true, false, true, false, true, false, true, true, true, false, false, false, false, true, false, false, false, false, false, false },
+            new bool[] { false, true, true, true, false, true, false, true, true, true, true, false, false, true, false, true, true, false, true, false, false, true, false, false, true, false, false, false, true, true, true, true, false, true, false, false, true, true, false, true, false, true, true, false, false },
+            new bool[] { false, false, true, true, true, true, true, true, true, true, false, false, true, true, false, true, false, false, false, false, true, true, true, true, true, false, true, false, true, false, false, true, false, true, true, false, true, true, true, true, true, false, true, true, true },
+            new bool[] { true, false, false, true, true, false, false, false, true, false, false, true, true, true, true, false, false, true, false, true, true, false, false, false, true, false, true, false, false, true, false, true, true, false, true, false, true, false, false, false, true, false, false, false, true },
+            new bool[] { true, true, false, true, true, false, true, false, true, false, true, true, false, true, false, false, false, true, true, true, true, false, true, false, true, true, true, true, false, false, false, false, true, false, true, true, true, false, true, false, true, false, true, false, true },
+            new bool[] { false, true, false, false, true, false, false, false, true, true, true, false, false, false, false, true, true, true, true, false, true, false, false, false, true, false, true, false, true, false, true, false, false, true, false, true, true, false, false, false, true, true, true, true, true },
+            new bool[] { false, true, true, true, true, true, true, true, true, true, false, false, true, true, true, true, false, true, false, false, true, true, true, true, true, true, true, false, false, true, false, true, false, false, false, false, true, true, true, true, true, true, false, false, false },
+            new bool[] { false, true, false, true, true, false, false, true, false, false, false, true, true, false, false, false, false, true, true, true, true, false, false, false, true, true, false, false, false, true, true, true, true, false, false, false, false, true, true, true, true, false, false, true, true },
+            new bool[] { false, true, false, false, true, true, true, false, true, true, true, true, false, false, false, false, true, true, false, true, false, false, false, false, false, true, true, false, true, true, false, true, false, false, false, false, true, true, false, true, false, true, true, true, false },
+            new bool[] { false, true, false, true, true, false, false, false, true, false, true, true, true, false, false, false, false, true, true, true, true, false, false, true, true, false, false, false, false, true, true, true, true, false, false, false, false, true, true, false, true, true, true, true, true },
+            new bool[] { true, true, true, true, false, false, true, false, false, false, false, true, true, true, false, true, false, false, true, true, true, true, false, false, false, false, true, true, false, false, true, true, true, true, false, true, false, false, true, false, false, false, false, true, true },
+            new bool[] { false, false, false, true, true, false, false, true, true, false, false, false, false, true, true, false, true, true, true, true, true, false, false, false, false, true, true, true, true, false, false, false, true, true, true, false, false, true, true, true, true, false, false, false, false },
+            new bool[] { true, false, true, true, false, true, true, false, true, true, false, true, false, false, false, true, false, true, true, false, true, true, false, true, false, false, false, false, true, true, false, false, false, true, true, true, false, false, false, false, true, true, false, true, false },
+            new bool[] { false, false, true, false, false, true, false, false, false, true, true, true, true, false, false, false, true, true, false, false, false, true, true, true, true, false, false, false, false, true, true, true, true, false, false, true, true, false, false, false, false, true, true, false, true },
+            new bool[] { false, false, false, false, true, true, true, false, false, false, true, false, true, true, false, true, true, false, true, false, false, false, true, false, true, true, false, false, false, false, true, true, true, false, false, false, true, true, false, false, false, false, true, false, true },
+            new bool[] { true, false, false, false, false, false, false, false, false, true, false, false, false, true, true, false, false, true, true, true, true, false, false, false, false, true, false, false, true, true, false, true, true, false, false, false, false, true, true, true, false, true, false, false, false },
+            new bool[] { false, false, false, false, true, false, true, true, false, false, false, false, true, false, true, false, true, true, true, true, false, false, true, true, false, false, false, true, false, false, true, true, false, false, true, false, true, true, true, true, false, false, false, true, false },
+            new bool[] { false, true, true, true, true, false, false, false, true, false, false, true, false, true, true, false, true, true, true, true, true, false, true, true, true, false, true, true, true, true, false, false, false, true, true, false, false, false, true, true, false, false, false, true, true },
+            new bool[] { true, false, false, true, true, false, true, true, true, true, false, false, true, true, true, true, false, true, false, true, true, true, true, true, true, true, false, true, true, false, false, false, true, true, false, true, true, true, true, true, true, true, true, true, false },
+            new bool[] { false, false, false, false, false, false, false, false, true, false, true, true, false, false, true, true, true, false, false, false, true, false, false, false, true, false, false, true, false, true, false, false, true, false, true, true, true, false, false, false, true, true, true, false, false },
+            new bool[] { true, true, true, true, true, true, true, false, true, true, false, true, false, true, false, true, false, false, false, false, true, false, true, false, true, true, false, false, true, true, false, true, true, true, false, false, true, false, true, false, true, true, false, false, true },
+            new bool[] { true, false, false, false, false, false, true, false, true, true, false, true, true, false, false, false, false, true, true, true, true, false, false, false, true, false, true, false, false, true, true, false, true, false, false, true, true, false, false, false, true, false, false, true, true },
+            new bool[] { true, false, true, true, true, false, true, false, true, false, true, true, true, true, false, true, false, false, true, false, true, true, true, true, true, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, false, false, false, true },
+            new bool[] { true, false, true, true, true, false, true, false, true, true, false, false, false, true, true, false, false, false, false, true, true, false, true, true, false, true, false, false, false, false, false, true, true, true, true, false, false, true, true, false, false, false, false, false, false },
+            new bool[] { true, false, true, true, true, false, true, false, false, false, true, false, false, false, true, true, true, true, false, false, true, true, true, false, false, true, true, false, false, true, false, false, true, false, true, true, true, false, true, false, true, true, false, false, false },
+            new bool[] { true, false, false, false, false, false, true, false, false, false, true, true, true, true, true, false, false, false, false, true, true, true, true, false, false, false, true, false, true, false, false, true, true, true, true, false, false, true, true, true, false, true, false, false, false },
+            new bool[] { true, true, true, true, true, true, true, false, true, true, false, false, true, true, true, true, false, true, false, false, true, false, false, true, false, true, false, true, false, true, false, false, true, true, true, true, false, false, false, true, false, false, false, false, true }
+        };
+    }
+}
